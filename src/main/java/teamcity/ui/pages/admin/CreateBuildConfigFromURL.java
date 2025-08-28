e232e2c9cbaf264b4be165f421cf896d85b376a4
Petr li <belarus.petr@gmail.com>
2025-08-28 17:14:40 +0200

    Base for UI tests - fixes for tests

--- src/main/java/teamcity/ui/pages/admin/CreateBuildConfigFromURL.java
+++ src/main/java/teamcity/ui/pages/admin/CreateBuildConfigFromURL.java
@@ -12,14 +12,26 @@ public class CreateBuildConfigFromURL extends CreateBasePage {
     private SelenideElement buildConfigName = $("#buildTypeName");
     private SelenideElement defaultBranch = $("#branch");
     private SelenideElement proceedBtn = $(By.cssSelector("input[value='Proceed']"));
+    private SelenideElement createDuplicateBtn = $(By.cssSelector("input[value='Create Duplicate VCS Root']"));
 
     public static CreateBuildConfigFromURL getCurrentPage() {
         return page(CreateBuildConfigFromURL.class);
     }
 
-    public void checkRequiredFieldsNotEmpty(){
+    public void checkRequiredFieldsNotEmpty(String buildConfigNameStr){
+        buildConfigName.clear();
+        buildConfigName.val(buildConfigNameStr);
+
         buildConfigName.shouldNotBe(Condition.empty);
         defaultBranch.shouldNotBe(Condition.empty);
+    }
+
+    public boolean checkIfDuplicate(){
+        return createDuplicateBtn.exists();
+    }
+
+    public void clickCreateDuplicateBtn(){
+        createDuplicateBtn.click();
     }
 
     public void clickProceedBtn(){
--- src/test/java/teamcity/ui/CreateProjectTest.java
+++ src/test/java/teamcity/ui/CreateProjectTest.java
@@ -5,6 +5,8 @@ import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 import teamcity.api.enums.Endpoint;
 import teamcity.api.generators.TestDataGenerator;
+import teamcity.api.models.Build;
+import teamcity.api.models.BuildType;
 import teamcity.api.models.Project;
 import teamcity.api.models.Step;
 import teamcity.api.requests.unchecked.UncheckedBase;
@@ -15,6 +17,7 @@ import teamcity.ui.pages.admin.CreateBuildConfigurationPage;
 import teamcity.ui.pages.admin.CreateProjectPage;
 
 import static io.qameta.allure.Allure.step;
+import static teamcity.api.enums.Endpoint.BUILD_TYPES;
 
 @Test(groups = {"Regression"})
 public class CreateProjectTest extends BaseUiTest {
@@ -47,100 +50,93 @@ public class CreateProjectTest extends BaseUiTest {
 
         softy.assertTrue(foundProjects);
 
-
-        // проверка состояния API
-        // (корректность отправки данных с UI на API)
-        step("Check that all entities (project, build type) was successfully created with correct data on API level");
-
-        // проверка состояния UI
-        // (корректность считывания данных и отображение данных на UI)
-        step("Check that project is visible on Projects Page (http://localhost:8111/favorite/projects)");
-
-        // взаимодействие с UI
-        step("Open `Create Project Page` (http://localhost:8111/admin/createObjectMenu.html)");
-        step("Send all project parameters (repository URL)");
-        step("Click `Proceed`");
-        step("Fix Project Name and Build Type name values");
-        step("Click `Proceed`");
-
-        // проверка состояния API
-        // (корректность отправки данных с UI на API)
-        step("Check that all entities (project, build type) was successfully created with correct data on API level");
-
-        // проверка состояния UI
-        // (корректность считывания данных и отображение данных на UI)
-        step("Check that project is visible on Projects Page (http://localhost:8111/favorite/projects)");
     }
 
     @Test(description = "User should be able to create build configuration", groups = {"Positive"})
     public void userCreatesProjectBuildConfiguration() {
 
         loginAs(testData.getUser());
-
         CreateProjectPage.open("_Root")
                 .createForm(REPO_URL)
                 .setupProject(testData.getProject().getName(), testData.getBuildType().getName());
 
-        var createdProject = superUserCheckedRequests.<Project>getRequest(Endpoint.PROJECTS).read("name:" + testData.getProject().getName());
-        softy.assertNotNull(createdProject);
+        var createdProject = superUserCheckedRequests.<Project>getRequest(Endpoint.PROJECTS)
+                .read("name:" + testData.getProject().getName());
+        softy.assertNotNull(createdProject, "Project creation failed");
 
         ProjectPage.open(createdProject.getId())
                 .title.shouldHave(Condition.exactText(testData.getProject().getName()));
-
         ProjectPage.addNewBuildConfiguration();
 
+        var buildData = TestDataGenerator.generate(Build.class);
         CreateBuildConfigurationPage.getCurrentPage()
-                        .createFormBuild(REPO_URL);
+                .createFormBuild(REPO_URL);
+        var currentBuildConfigPage = CreateBuildConfigFromURL.getCurrentPage();
+        currentBuildConfigPage.checkRequiredFieldsNotEmpty(buildData.getBuildType().getName());
+        currentBuildConfigPage.clickProceedBtn();
 
-        CreateBuildConfigFromURL.getCurrentPage()
-                        .checkRequiredFieldsNotEmpty();
-        CreateBuildConfigFromURL.getCurrentPage()
-                .clickProceedBtn();
+        if (currentBuildConfigPage.checkIfDuplicate()) {
+            currentBuildConfigPage.clickCreateDuplicateBtn();
+        }
 
+        softy.assertTrue(ProjectPage.getCurrentPage().buildStepsActive(), "Build steps not active");
 
-        softy.assertTrue(ProjectPage.getCurrentPage().buildStepsActive());
+        BuildType createdBuildType = superUserCheckedRequests.<BuildType>getRequest(BUILD_TYPES)
+                .read("name:" + buildData.getBuildType().getName());
+        softy.assertEquals(testData.getBuildType().getName(), createdBuildType.getName(),
+                "Build type name mismatch between test data and API");
 
     }
 
 
     @Test(description = "User should be able to create build and add new steps with echo \"Hello, World!\"", groups = {"Positive"})
-    public void userCreatesProjectWithCommandLineBuildStep() {
+    public void userCreatesProjectWithCommandStep() {
+        var unchecked = new UncheckedBase(Specifications.superUserAuth(), Endpoint.PROJECTS);
 
-        var uncheckedProjects = new UncheckedBase(Specifications.superUserAuth(), Endpoint.PROJECTS);
-        var projectsResponse = uncheckedProjects.read("");
+        var projectsResponse = unchecked.read("");
         projectsResponse.then().assertThat().statusCode(HttpStatus.SC_OK);
+        String firstProjectName = projectsResponse.jsonPath().getString("project[1].name");
+        String firstProjectId = projectsResponse.jsonPath().getString("project[1].id");
+        softy.assertNotNull(firstProjectName, "First project name");
 
-        var firstProjectName = projectsResponse.jsonPath().getString("project[1].name");
-        var firstProjectId = projectsResponse.jsonPath().getString("project[1].id");
-        softy.assertNotNull(firstProjectName, "First project name should not be null");
-        step("Fetched first project name: " + firstProjectName);
-
-        var uncheckedBuildTypes = new UncheckedBase(Specifications.superUserAuth(), Endpoint.BUILD_TYPES);
-        var buildTypesResponse = uncheckedBuildTypes.read("?locator=project:" + firstProjectId);
+        var buildTypesResponse = unchecked.read("?locator=project:" + firstProjectId);
         buildTypesResponse.then().assertThat().statusCode(HttpStatus.SC_OK);
+        String firstBuildTypeId = buildTypesResponse.jsonPath().getString("buildType[0].id");
+        softy.assertNotNull(firstBuildTypeId, "First build type ID"); // Records failure but doesn't stop test yet
 
-        var firstBuildTypeId = buildTypesResponse.jsonPath().getString("buildType[0].id");
-        softy.assertNotNull(firstBuildTypeId, "First build type name should not be null");
+        if (firstBuildTypeId == null) {
+            step("No existing build type found, creating a new one via API");
+            var buildData = TestDataGenerator.generate(Build.class);
+            buildData.getBuildType().setProject(new Project(firstProjectId, firstProjectName, null, null)); // Link to existing project
+            var createdBuildType = superUserCheckedRequests.<BuildType>getRequest(Endpoint.BUILD_TYPES)
+                    .create(buildData.getBuildType());
+            firstBuildTypeId = createdBuildType.getId();
+            softy.assertNotNull(firstBuildTypeId, "Newly created build type ID");
+            org.testng.Assert.assertNotNull(firstBuildTypeId, "Critical: Failed to create build type via API");
+        }
+
         var step = TestDataGenerator.generate(Step.class);
 
-        step("Fetched first build type name: " + firstBuildTypeId);
-
         loginAs(testData.getUser());
-
         EditBuildPage.openEditBuildRunners(firstBuildTypeId)
-                        .createNewBuildSteps(step.getName(), step.getId(), "echo \"Hello, World!\"");
-
+                .createNewBuildSteps(step.getName(), step.getId(), "echo \"Hello, World!\"");
         BuildConfigPage.getCurrentPage()
-                        .checkThatStepBuildCreated(step.getName(), "echo \"Hello, World!\"");
+                .checkThatStepBuildCreated(step.getName(), "echo \"Hello, World!\"");
 
+        var buildTypeStepsResponse = new UncheckedBase(Specifications.superUserAuth(), Endpoint.BUILD_TYPES)
+                .read(firstBuildTypeId + "/steps");
+        buildTypeStepsResponse.then().assertThat().statusCode(HttpStatus.SC_OK);
 
+        String createdStepName = buildTypeStepsResponse.jsonPath().getString("step.find { it.name == '" + step.getName() + "' }.name");
+        String createdStepScript = buildTypeStepsResponse.jsonPath().getString("step.find { it.name == '" + step.getName() + "' }.properties.property.find { it.name == 'script.content' }.value");
+        softy.assertEquals(createdStepName, step.getName(), "Step name mismatch");
+        softy.assertEquals(createdStepScript, "echo \"Hello, World!\"", "Step script content mismatch");
     }
 
 
     @Test(description = "User should not be able to craete project without name", groups = {"Negative"})
     public void userCreatesProjectWithoutName() {
 
-        // подготовка окружения
         step("Login as user");
         loginAs(testData.getUser());
 
@@ -148,43 +144,22 @@ public class CreateProjectTest extends BaseUiTest {
         var projectsCount = superUserCheckedRequests.<Project>getRequest(Endpoint.PROJECTS)
                 .read("").getCount();
 
-        // взаимодействие с UI
+
         step("Open `Create Project Page`[](http://localhost:8111/admin/createObjectMenu.html)");
         CreateProjectPage.open("_Root")
                 .createForm(REPO_URL)
                 .setupProject("", testData.getBuildType().getName());
 
-        // проверка состояния API
-        // (корректность отправки данных с UI на API)
+
         step("Check that number of projects did not change");
         var finalProjectsCount = superUserCheckedRequests.<Project>getRequest(Endpoint.PROJECTS)
                 .read("").getCount();
         softy.assertEquals(projectsCount, finalProjectsCount);
 
-        // проверка состояния UI
-        // (корректность считывания данных и отображение данных на UI)
+
         step("Check that error appears `Project name must not be empty`");
         ProjectsPage.validateErrorMessage();
 
 
-
-        // подготовка окружения
-        step("Login as user");
-        step("Check number of projects");
-
-        // взаимодействие с UI
-        step("Open `Create Project Page` (http://localhost:8111/admin/createObjectMenu.html)");
-        step("Send all project parameters (repository URL)");
-        step("Click `Proceed`");
-        step("Set Project Name");
-        step("Click `Proceed`");
-
-        // проверка состояния API
-        // (корректность отправки данных с UI на API)
-        step("Check that number of projects did not change");
-
-        // проверка состояния UI
-        // (корректность считывания данных и отображение данных на UI)
-        step("Check that error appears `Project name must not be empty`");
     }
 }
